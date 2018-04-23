cd1869a3a52670de1d9c899022e9bfbee1d5ed06
huynguyen77 <quanghuynguyen040197@gmail.com>
2018-04-23 19:11:48 +0700

    xdff

---
+++ src/P01/Payable.java
@@ -0,0 +1,5 @@
+package P01;
+
+public interface Payable {
+	 double getPaymentAmount(); // calculate payment; no implementation
+}
---
+++ src/P01/Txt.java
@@ -0,0 +1,7 @@
+package P01;
+
+public class Txt {
+	// su dung mang de truyen tham chieu
+	// IntHolder ... kieu tham chieu thu vien org.omg.CORBA
+
+}
